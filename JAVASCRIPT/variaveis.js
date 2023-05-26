8197ef50a01e72d54e48a36ba313fded642b17ff
androb86 <ab.familia.silvaaraujo@gmail.com>
2023-05-26 17:08:46 -0300

    iniciandojs

---
+++ JAVASCRIPT/variaveis.js
@@ -0,0 +1,30 @@
+let idade = 36;
+console.log(idade)
+
+
+//Tipos de variaveis
+
+idade = 30;       //number
+let numero = 30;  //number
+let altura = 177; //number
+let nome = "Robson"  //string
+let estudando = true  //booleano (true o false)
+
+
+console.log(numero, typeof numero);
+console.log(altura, typeof altura);
+console.log(nome, typeof nome);
+console.log(estudando, typeof estudando)
+
+let outra;
+
+console.log(outra);  //undefined
+
+let nula = null;
+
+console.log(nula);
+
+var variavel = 30;  //versão mais antiga 
+const constante = 100; //unico valor e não altera mais
+
+console.log(constante);
