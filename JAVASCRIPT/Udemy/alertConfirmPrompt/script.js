cc928e9a74899710cda159f3a7300d4421b7e211
androb86 <ab.familia.silvaaraujo@gmail.com>
2023-08-21 08:54:28 -0300

    revisando

--- JAVASCRIPT/Udemy/alertConfirmPrompt/script.js
+++ JAVASCRIPT/Udemy/alertConfirmPrompt/script.js
@@ -1,1 +1,9 @@
-alert('com a nossa mensagem.');
+let num1 = prompt('Digite um Numero: ');
+let num2 = prompt('Digite outro Numero: ');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resultado = num1 + num2;
+alert('O resultado da sua conta foi: ' + resultado);
+//alert(`O resultado da sua conta foi: ${resultado}`); template String
